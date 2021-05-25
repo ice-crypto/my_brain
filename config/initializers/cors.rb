a3980f0cb52fe6126f2b7924099944f0552cd8c5
ice-crypto <testtest2tom@gmail.com>
2021-05-25 17:21:44 +0900

    本番 cors変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '127.0.0.1:3001'
+    origins 'my-brain.site'
 
     resource '*',
       headers: :any,
